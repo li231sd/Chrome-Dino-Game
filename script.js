62d0d5b22fd761050cbd341be81f494c1037e90d
li231sd <113144462+li231sd@users.noreply.github.com>
2022-09-23 19:02:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
+var delayInMilliseconds = 5000; //5 second
+
 function jump() {
     if (dino.classList != "jump") {
         dino.classList.add("jump");
@@ -11,20 +13,22 @@ function jump() {
     }
 }
 
-let isAlive = setInterval(function() {
-    //Get current dino Y pos
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    //Get current cactus X pos
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-    
-    //Detect Collision
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140)
-    {
-        //collision
-        alert("Game Over") 
-    }
-}, 10)
-
 document.addEventListener("keydown", function(event) {
     jump();
 })
+
+setTimeout(function() {
+    let isAlive = setInterval(function() {
+        //Get current dino Y pos
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        //Get current cactus X pos
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+        
+        //Detect Collision
+        if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140)
+        {
+            //collision
+            alert("Game Over") 
+        }
+    }, 10)
+}, delayInMilliseconds);
